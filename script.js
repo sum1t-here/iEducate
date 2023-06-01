b4f641b551e36775ee97a448f93f29ec3ac581d5
sum1t_here <mazumdarsumit3@gmail.com>
2023-06-01 22:22:39 +0530

    added code editor feature

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ burger.addEventListener("click", () => {
   navList.classList.toggle("v-class-resp");
   navbar.classList.toggle("h-nav-resp");
 });
+
+// for the editor
+
+function run() {
+  let html = document.getElementById("html").value;
+  let css = document.getElementById("css").value;
+  let js = document.getElementById("js").value;
+  let output = document.getElementById("output");
+
+  output.contentDocument.body.innerHTML = html + "<style>" + css + "</style>";
+  output.contentWindow.eval(js);
+}
